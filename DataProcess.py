f94bd89273962ccfa9069544eab64550f86fad74
YHanchao <1405668636@qq.com>
2021-08-05 19:39:14 +0800

    0805_19_39_YAN

---
+++ DataProcess.py
@@ -0,0 +1,29 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+allDf = pd.read_csv('Data\\OriginData.csv')
+allDf = allDf[pd.notna(allDf['品牌类型'])]  # 把原数据中1969行之后本不应该存在的数据删掉
+
+# 数据可视化：频率分布直方图
+for i in list(allDf)[2: -1]:
+    distance = 10   # 组距
+    if i[0] == 'a':
+        group_num = int((max(allDf[i]) - min(allDf[i])) / distance)
+        plt.hist(allDf[i], bins=group_num)
+    else:
+        plt.hist(allDf[i])
+    plt.savefig('Fig\\' + i + '.png')
+    plt.close()
+
+
+# 数据预处理：拆分三款汽车
+
+dfType1 = allDf[allDf['品牌类型'] == 1]
+dfType2 = allDf[allDf['品牌类型'] == 2]
+dfType3 = allDf[allDf['品牌类型'] == 3]
+
+dfType1.to_csv('Data\\Type1.csv', index = False)
+dfType2.to_csv('Data\\Type2.csv', index = False)
+dfType3.to_csv('Data\\Type3.csv', index = False)
